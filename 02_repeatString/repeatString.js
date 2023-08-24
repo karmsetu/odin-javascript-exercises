77217245af9519199e4e06c3b3d1b035d7697da8
Karmsetu <141593846+karmsetu@users.noreply.github.com>
2023-08-24 07:08:20 +0530

    completed excercise 01 - 03 with some help, doing in bits

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if(num <= 0){return Error}
+    mdfString = ""
+    for (let index = 0; index < num; index++) {
+        mdfString += string
+        
+    }
+    return mdfString
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    // let revStr
+    // if (str == null) { 
+    //     return Error    
+    // }
+    // for (let index = 1; index == str.length; index++) {
+    //     revStr += str[str.length - index]
+    // }
+    // return revStr
+    return str.split("").reverse().join("");
+    // i dont understand a word (lol)
 };
 
 // Do not edit below this line
