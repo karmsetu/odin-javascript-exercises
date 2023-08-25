3c5be38ca6182efce310b5a2071834014cd7f86b
Karmsetu <141593846+karmsetu@users.noreply.github.com>
2023-08-26 01:12:48 +0530

    date: 26.08.23

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let firstPrev = 1;
+    let secondPrev = 0;
+    
+    for (let i = 2; i <= num; i++) {
+        let current = firstPrev + secondPrev;
+        secondPrev = firstPrev;
+        firstPrev = current;
+    }
 
+    return firstPrev;
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
+
+// 0,1,1,2,3,5,8
+
+// for (index->0,1,2,3 ,num= 4):
+//     1+1
+
+// 1+1+2+3
+
+// precedingnum = sum 
