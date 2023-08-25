90036f7908adeb13f40ad27450e2359c594cc4d4
Karmsetu <141593846+karmsetu@users.noreply.github.com>
2023-08-26 02:05:28 +0530

    the basic level has been completed, next aim is to pass all the parameter!

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,33 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(input) {
+    nameAndAgeArray = []
+    let oldest
+    age = []
+    input.forEach(element => {
+        // somwObj = new obj
+        someObj = []
+        someObj.name = element.name
+        someObj.age = element.yearOfDeath - element.yearOfBirth
+        // nameAndAgeArray[element.name] =  element.yearOfDeath - element.yearOfBirth
+        // return element.name
+        nameAndAgeArray.push(someObj)
+    });
+    nameAndAgeArray.forEach(element=>{
+        age.push(element.age) 
+    });
+    // console.log(age)
+    max = Math.max(...age)
+    nameAndAgeArray.forEach(element=>{
+        if(element.age == max){
+            oldest = element
+            console.log(oldest)
+        } else{
+            // console.log(element)
+        }
+    })
+    return oldest
+    // console.log(`max:${Math.max(...age)}`)
+    // nameAndAgeArray.
+    // return nameAndAgeArray
 };
 
 // Do not edit below this line
